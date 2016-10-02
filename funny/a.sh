075a5e16e10c5e0a21c7f20b35b1c2b4c5f2b5ca
ikatake <ikatake+fromgithub@gmail.com>
2016-10-02 23:52:26 +0900

    remove temporary files

--- funny/a.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-curl -n -X GET -H "Authorization: Bearer 3df61022350585b2e5a2890ef9cdd2201ef5897c50f655cd618fe90b8bac3c76" https://suzuri.jp/api/v1/activities \
--G \
- -d limit=30
-#curl -n -X GET -H "Authorization: Bearer 3df61022350585b2e5a2890ef9cdd2201ef5897c50f655cd618fe90b8bac3c76" https://suzuri.jp/api/v1/items
-#-H "Authorization: Bearer 3df61022350585b2e5a2890ef9cdd2201ef5897c50f655cd618fe90b8bac3c76"\
-#curl -n -X GET https://suzuri.jp/api/v1/schema
-
