3088998df76c28e15e3a99e3a1508a29793c202c
ikatake <ikatake@gmail.com>
2017-01-14 16:47:50 +0900

    add func for measure size and validation

--- lgb_util.rb
+++ lgb_util.rb
@@ -22,4 +22,65 @@ def get_state_text(gene, step)
   return r
 end
 
+def get_lastest_gene_step()
+  filename = "/home/ikatake/local/twlg/state.txt"
+  file = File.open(filename)
+  text = file.read
+  file.close
+  lines = text.split(/\n/)
+  arr = lines[10].split(/\t/)
+  gene = arr[2]
+  arr = lines[11].split(/\t/)
+  step = arr[2]
+  p gene
+  p step
+  arr = [gene, step]
+  return arr
+end
+
+def get_lastest_state_test()
+  filename = "/home/ikatake/local/twlg/state.txt"
+  file = File.open(filename)
+  text = file.read
+  file.close
+  lines = text.split(/\n/)
+  r = "";
+  10.times do |i|
+    r += (lines[i] + '\n')
+  end
+  return r
+end
+
+def is_valid_gene_step?(gene, step)
+  arr = get_lastest_gene_step()
+  cur_gene = arr[0]
+  cur_step = arr[1]
+  if gene > cur_gene
+    return false
+  end
+  if gene == cur_gene
+    if step > cur_step
+      return false
+    else
+      return true
+    end
+  end
+  if step > measure_gene(gene)
+    return false
+  else
+    return true
+  end
+end
+
+def measure_gene(gene)
+  dir_path = 'home/ikatake/www/wetsteam/lifegamebot/stateLogs/'
+  dir_path << sprintf("%08d/", gene) 
+  if ( FileTest.exist?(dir_path) && Filetest.directory?(dir_path) ) == false
+    return -1;
+  end
+  arr = Dir.glob(dir_path & "[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9].txt")
+  return arr.size
+end
+  
+
 
