444505756d7545af8fad7d165e7112fd2b626353
ikatake <ikatake@gmail.com>
2017-12-03 23:26:26 +0900

    add testing script to make sticker on suzuri

--- suzuri_util.rb
+++ suzuri_util.rb
@@ -1,66 +1,45 @@
-#!/usr/bin/ruby
-require 'rmagick'
-require 'time'
-require 'fileutils'
+#!/bin/ruby
 
-def convert_img_sticker(state, mode)
-  size_cell = 50;
-  spc_cell = 10;
-  num_cells = 10;
-  spc_frame = 20;
-  width_stroke = 3;
-  len_img = size_cell * num_cells + spc_cell * ( num_cells - 1 ) + 2 * spc_frame
-  canvas = Magick::ImageList.new
-  if(mode == 'black')
-    canvas.new_image(len_img, len_img) { self.background_color = "black" }
-  else
-    canvas.new_image(len_img, len_img) { self.background_color = "white" }
-  end
-  arr_state = state.split(/\\n/)
-  p arr_state
-  
-  num_cells.times do |y|
-    y0 = spc_frame + y * ( size_cell + spc_cell )
-    y1 = y0 + size_cell
-    num_cells.times do |x|
-      x0 = spc_frame + x * ( size_cell + spc_cell )
-      x1 = x0 + size_cell
-      s = arr_state[y][x]  #セルの状態を得る
-      rect_cell = Magick::Draw.new
-      rect_cell.rectangle(x0, y0, x1, y1)
-      if(mode == 'black' && s == '1') #塗潰しの標準は黒。ON状態のみ、緑にする。
-        rect_cell.fill = "green"
-      elsif(mode == 'white' && s == '0') #同様にOFF状態のみ白にする。
-        rect_cell.fill = "white"
-      end
-      rect_cell.stroke_width = width_stroke
-      if(mode == 'black')
-        rect_cell.stroke = "green"
-      elsif(mode == 'white')
-        rect_cell.stroke = "black"
-      end
-      rect_cell.draw(canvas)
-    end
-  end
-  file_path = set_file_name_sticker()
-  canvas.write(file_path);
-  r = file_path
+require 'json';
+require 'faraday'
+
+conn = Faraday::Connection.new(:url => 'https://suzuri.jp/') do
+|builder|
+  builder.use Faraday::Request::UrlEncoded
+  builder.use Faraday::Response::Logger
+  builder.use Faraday::Adapter::NetHttp
 end
 
-def set_file_name_sticker()
-  ymdhms = Time.now.strftime("%Y%m%d%H%M%S")
-  file_name = "stkr" + ymdhms + ".png"
-  dir_name = ENV['HOME'] + "/www/wetsteam/lifegamebot/tmpimg"
-  if( !FileTest::exist?(dir_name) )
-    FileUtils.mkdir_p(dir_name)
-  end
-  path = dir_name + "/" + file_name
-end
-  
+#response = conn.get do |request|
+#  request.url  'api/v1/items'
+#  request.headers['Authorization'] = 'Bearer 3df61022350585b2e5a2890ef9cdd2201ef5897c50f655cd618fe90b8bac3c76'
+#  request.headers['Content-Type'] = 'application/json'
+##  request.body = JSON.generate(param)
+#end
+#json = JSON.parser.new(response.body)
+#p json.parse
 
-def send_wear(png, gene, step)
-  p png
-  p gene
-  p step
+response = conn.post do |request|
+  request.url  'api/v1/materials'
+  request.headers['Authorization'] = 'Bearer 3df61022350585b2e5a2890ef9cdd2201ef5897c50f655cd618fe90b8bac3c76'
+  request.headers['Content-Type'] = 'application/json'
+  request.body = '{
+    "title": "@_lifegamebot g:1 s:1",
+    "texture": "http://www.wetsteam.org/lifegamebot/tmpimg/1_1.png",
+    "price": 0,
+    "description": "@_lifegamebot g:1 s:1 sticker",
+    "products": 
+    [{
+      "itemId": 11,
+      "published": true,
+      "resizeMoed": "contain"
+    }]
+  }'
 end
+#p response.body
+json = JSON.parser.new(response.body)
+p "\n===\n"
+
+p json.parse
+
 
