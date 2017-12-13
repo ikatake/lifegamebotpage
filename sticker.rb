e9ca22bab4709c0083020c8dd4c4d285d50a110f
ikatake <ikatake@gmail.com>
2017-12-13 21:57:43 +0900

    change image library

--- sticker.rb
+++ sticker.rb
@@ -1,4 +1,4 @@
-#!usr/local/bin/ruby
+#!/usr/bin/env /home/ikatake/local/rbenv/shims/ruby
 
 ##!/home/ikatake/local/rbenv/shims/ruby
 
@@ -8,8 +8,7 @@ require_relative './suzuri_util.rb'
 require_relative './lgb_util.rb'
 require 'cgi'
 require 'date'
-require 'cairo'
-require 'faraday'
+require 'chunky_png'
 
 print "Content-Type: text/html\n\n"
 print "<html><head><title>nyan</title></head><body>\n"
@@ -40,7 +39,8 @@ end
 if (cgi.has_key?('gene') && cgi.has_key?('step'))
   gene = cgi['gene'].to_i
   step = cgi['step'].to_i
-  p measure_gene(gene)
+	p gene
+	p step
   if( is_valid_gene_step?(gene, step) == false)
     print "invalid gene or step."
     print "</body></html>\n"
@@ -55,63 +55,83 @@ else #nothing gene & step => get lastst state
 end
 
 #make iamge file
-file_name = "tmp/" + gene.to_s + "_" + step.to_s + "_"
+file_name = "sticker_img/" + gene.to_s + "_" + step.to_s + "_"
 file_name += Time.now.to_i.to_s + ".png"
 	p file_name
-format = Cairo::FORMAT_ARGB32
 width = 630
 height = 630
-surface = Cairo::ImageSurface.new(format, width, height)
-context = Cairo::Context.new(surface)
+if(color == "white")#fill bg clear(white)
+  png = ChunkyPNG::Image.new(width, height, ChunkyPNG::Color::TRANSPARENT)
+elsif(color == "black") #fill bg color(black)
+  png = ChunkyPNG::Image.new(width, height, ChunkyPNG::Color::BLACK)
+end
 
 cell_size = 50
 cell_margin = 10
-line_width = 2
-top_margin = 15
-left_margin = 15
 line_width = 4
+top_margin = (height - cell_margin * 9 - cell_size * 10) / 2
+left_margin = (width - cell_margin * 9 - cell_size * 10) / 2
+color_green = ChunkyPNG::Color.rgb(0, 255, 0)
+x0 = top_margin
+y0 = left_margin
 
-#fill background color(black only)
-context.rectangle(0, 0, width, height)
-context.set_source_rgb(0, 0, 0,)
-context.fill
-
-context.translate(top_margin, left_margin)
-context.set_line_width(line_width)
 arr = state.split(/\\n/)
 for line in arr do
   charr = line.split("")
   for ch in charr do
-    context.rectangle(0, 0, cell_size, cell_size)
-    if(ch == "0")
-      #draw white rectangle
-      #context.set_source_rgb(1, 1, 1)
-    elsif(ch == "1")
-      #draw black rectangle
-      if(color == "white") 
-        context.set_source_rgb(0, 0, 0)
-      elsif(color == "black")
-        context.set_source_rgb(0, 1, 0)
-      end
-      context.fill_preserve
-    end
-    if(color == "white") 
-      context.set_source_rgb(0, 0, 0)
+    #draw out rectangle
+    x1 = x0 + cell_size
+    y1 = y0 + cell_size
+    if(color == "white")
+      color_o = ChunkyPNG::Color::BLACK
     elsif(color == "black")
-      context.set_source_rgb(0, 1, 0)
+      color_o = color_green
     end
-    context.stroke
+    png.rect(x0, y0, x1, y1, color_o, color_o)
+    
+    #draw inside rectangle
+    x0i = x0 + line_width
+    y0i = y0 + line_width
+    x1i = x1 - line_width
+    y1i = y1 - line_width
+    if(ch == "0" && color == "white")
+      #draw black stroke & no rect
+      color_i = ChunkyPNG::Color::WHITE
+    elsif(ch == "1" && color == "white")
+      #draw black stroke & black rect
+      color_i = ChunkyPNG::Color::BLACK
+    elsif(ch == "0" && color == "black")
+      #draw green stroke & black rect
+      color_i = ChunkyPNG::Color::BLACK
+    elsif(ch == "1" && color == "black")
+      #draw green stroke & green rect
+      color_i = color_green
+    end
+    png.rect(x0i, y0i, x1i, y1i, color_i, color_i)
     # 1セル分右にずらす
-    context.translate( (cell_size + cell_margin), 0)
+    x0 = x0 + cell_size + cell_margin
   end
   #左右位置を元に戻して、1段下にずらす。
-  context.translate( -(cell_size + cell_margin) * charr.length, 0)
-  context.translate( 0, (cell_size + cell_margin) )
+  x0 = top_margin
+  y0 = y0 + cell_size + cell_margin
 end
+png.save(file_name)
 
-surface.write_to_png(file_name)
-
-suzuri("http://www.wetsteam.org/lifegamebot/tmpimg/1_1.png", gene, step, "sticker")
+img_address = "http://www.wetsteam.org/lifegamebot/" + file_name
+print %Q{<span style="color:white"}
+ret = suzuri( img_address, gene, step, "sticker")
+print %Q{</span>}
+p ret
+if(ret[0] == "3" || ret[0] == "4" || ret[0] == "5")
+  print "</br>生成に失敗しました。さようなら。</br>\n"
+  print %Q{<script type="text/javascript">\n}
+  print %Q{<!-- \nwindow.open('about:_blank','_self').close()\n -->\n}
+  print %Q{</script>\n}
+else
+  print %Q{<script type="text/javascript">\n}
+  print %Q{<!-- \nwindow.location.href='#{ret}'\n -->\n}
+  print %Q{</script>\n}
+end
 print "</body></html>\n"
 #send to suzuri
 
