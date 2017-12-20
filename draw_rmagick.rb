f9776e62ab96fee7fb7261117e611ecec44e992f
ikatake <ikatake@gmail.com>
2017-12-20 23:33:02 +0900

    add draw_rmagick.rb that is drawing lifegame image to image object.

---
+++ draw_rmagick.rb
@@ -0,0 +1,12 @@
+#!/home/ikatake/local/rbenv/shims/ruby
+
+# img		Macick::Image object
+# size		cell size[px]
+# margin	margin between celluer[px]
+# width		stroke width[px]
+# x0, y0	initial position of drawing[px](first rect north-west point)
+# bg_color	background color ex.:"#000000"
+# fr_color	front color ex.:"#ffffff"
+# state		lifegame state text
+def draw_field(img, size, margin, width, x0, y0, bg_color, fr_color, state)
+
