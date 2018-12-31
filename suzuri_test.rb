1d6b66a34cc91fb309a849030e2a7597ca02a1e8
ikatake <ikatake@gmail.com>
2018-12-31 21:25:14 +0900

    :q

--- suzuri_test.rb
+++ suzuri_test.rb
@@ -3,7 +3,7 @@
 require 'json';
 require 'faraday'
 
-conn = Faraday::Connection.new(:url => 'https://suzuri.jp/', :ssl => {:verify => false}) do |builder|
+conn = Faraday::Connection.new(:url => 'https://suzuri.jp/', :ssl => {:ca_file => "cacert.pem"}) do |builder|
   builder.use Faraday::Request::UrlEncoded
   builder.use Faraday::Response::Logger
   builder.use Faraday::Adapter::NetHttp
